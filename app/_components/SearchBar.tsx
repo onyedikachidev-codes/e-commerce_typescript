2570a31c3c641f9665ebeb2466fba5e058d297b1
Onyedikachi Nwanguma <nwangumabimma@gmail.com>
2025-04-13 23:39:33 +0100

    search

--- app/_components/SearchBar.tsx
+++ app/_components/SearchBar.tsx
@@ -21,7 +21,7 @@ export default function SearchBar() {
           className="w-full placeholder-blue-400 border-b relative border-gray-500 focus:outline-none focus:border-gray-600 px-3 py-2 transition-transform focus:scale-101 duration-200 ease-in-out"
           onChange={handleSearch}
         />
-        <button className="absolute right-3 top-1/2 transform -translate-y-1/2 text-blue-700 hover:text-gray-500">
+        <button className="absolute right-3 top-1/2 transform -translate-y-1/2 text-blue-700 hover:text-gray-500 cursor-pointer">
           <CiSearch size={20} />
         </button>
       </div>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,7 +5,6 @@ import axios, { AxiosResponse } from "axios";
 import { ProductListingProps } from "../_models/item";
 import ProductListingItem from "../_components/ProductListingItem";
 import Header from "../_components/Header";
-import { IoIosArrowDown } from "react-icons/io";
 
 import { Montserrat } from "next/font/google";
 import { useQuery } from "@tanstack/react-query";
@@ -30,7 +29,7 @@ const fetchProducts = async (): Promise<ProductListingProps[]> => {
 };
 
 export default function Page() {
-  const [value, setValue] = useState("Search products...");
+  const [value, setValue] = useState("Search products, Brands and Categories");
   const [errorText, setErrorText] = useState("");
 
   const debouncedSearch = useDebounce(value, 700);
@@ -39,7 +38,8 @@ export default function Page() {
     queryKey: ["search", debouncedSearch],
     queryFn: () => fetchSearchedProducts(debouncedSearch),
     enabled:
-      debouncedSearch.length >= 4 && debouncedSearch !== "Search products...",
+      debouncedSearch.length >= 4 &&
+      debouncedSearch !== "Search products, Brands and Categories",
   });
 
   const {
@@ -51,7 +51,11 @@ export default function Page() {
     queryKey: ["products"],
     queryFn: fetchProducts,
   });
-  const totalItems: number = products?.length ?? 0;
+
+  const totalItems =
+    value === "Search products, Brands and Categories" || value.length < 4
+      ? products?.length ?? 0
+      : filteredProducts?.length ?? 0;
 
   if (isLoading) return <Spinner />;
   if (isError) return <div>Error: {error.message}</div>;
@@ -59,34 +63,51 @@ export default function Page() {
   return (
     <>
       <Header />
-      <div
-        className={`${mons.className} flex justify-between items-center border-b border-gray-300 py-4 mt-24 mx-7 text-xl font-semibold`}
-      >
+      <div className="mt-24 flex justify-center items-center">
         <SearchBig
           value={value}
           setValue={setValue}
           errorText={errorText}
           setErrorText={setErrorText}
         />
+      </div>
+      <div
+        className={`${mons.className} flex justify-between items-center border-b border-gray-300 py-4  mx-7 text-xl font-semibold`}
+      >
         <h2>
           Shop with Trivela
           <span className="font-medium text-gray-600 text-lg">
-            ({totalItems} products found)
+            (&nbsp;{totalItems} products found )
           </span>
         </h2>
-        <h3 className="flex items-center">
-          Sort by: Popularity <IoIosArrowDown />
-        </h3>
+        <div className="border px-2.5 py-3 rounded-lg font-normal text-sm flex gap-8 items-center cursor-pointer focus:border-blue-400">
+          <p>Sort By</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5"
+            />
+          </svg>
+        </div>
       </div>
       <div
         className={`${mons.className} grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 py-6 mt-6 px-20`}
       >
-        {/* {(value.length >= 4 && filteredProducts
+        {(value !== "Search products, Brands and Categories" &&
+        value.length >= 4
           ? filteredProducts
           : products
-        ).map((product) => (
+        )?.map((product) => (
           <ProductListingItem key={product.id} {...product} />
-        ))} */}
+        ))}
       </div>
     </>
   );
